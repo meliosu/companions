fa3c08671d76263fadfdddd3231052d0ce83eb75
Alexander <alex.atyurin@gmail.com>
2024-11-11 00:36:46 +0700

    change main

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,23 @@
+use anyhow::anyhow;
+use backend::{config::Config, proto::companions_server::CompanionsServer, service::ApiService};
+use tonic::transport::Server;
+
 #[tokio::main]
 async fn main() {
-    todo!()
+    if let Err(e) = run().await {
+        eprintln!("Error: {e}");
+    }
+}
+
+async fn run() -> anyhow::Result<()> {
+    let config = Config::from_env()?;
+    let service = ApiService::new(&config.database_url).await?;
+
+    Server::builder()
+        .add_service(CompanionsServer::new(service))
+        .serve(config.address)
+        .await
+        .map_err(|e| anyhow!("while running server: {e}"))?;
+
+    Ok(())
 }
